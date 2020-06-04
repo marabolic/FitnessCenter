c4f1d3e1312bf1693997536a64f0090370d50be5
marabolic <maki.bolic@gmail.com>
2020-06-04 21:31:19 +0200

    Home page functional, needs styling

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import TrainingDetails from '../components/services/TrainingDetails.vue'
 
 Vue.use(VueRouter)
 
@@ -9,6 +10,11 @@ Vue.use(VueRouter)
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/trainingdetails/:id',
+    name:'Details',
+    component: TrainingDetails
   },
   {
     path: '/about',
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,23 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <WhyUs msg="Welcome to Your Vue.js App"/>
+    <TopRated msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import WhyUs from '@/components/homepage/WhyUs.vue'
+import TopRated from '@/components/homepage/TopRated.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    WhyUs,
+    TopRated
+  },
+  created(){
+      localStorage.setItem('lang', 'en');
   }
 }
 </script>
