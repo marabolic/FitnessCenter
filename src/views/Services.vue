b88d38776a76a65a78f41c07f0acd5ae1913199e
marabolic <maki.bolic@gmail.com>
2020-06-05 08:26:38 +0200

    added massage and nutritionist forms

--- src/views/Services.vue
+++ src/views/Services.vue
@@ -1,12 +1,22 @@
 <template>
     <div class= 'services'>
-        
+        <Programs msg="Welcome to Your Vue.js App"/>
+        <Massage msg="Welcome to Your Vue.js App"/>
+        <Nutritionist msg="Welcome to Your Vue.js App"/>
     </div>
 </template>
 
 
 <script>
+    import Programs from "../views/Programs"
+    import Massage from "../views/Massage"
+    import Nutritionist from "../views/Nutritionist"
     export default {
-            
+            name:"Services",
+            components:{
+                Programs,
+                Massage,
+                Nutritionist
+            }
     }
 </script>
