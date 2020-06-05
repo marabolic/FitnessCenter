8cf13da390e5d0300452179460b5c5bff11ea481
marabolic <maki.bolic@gmail.com>
2020-06-05 08:26:38 +0200

    added massage and nutritionist forms

---
+++ src/views/Massage.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class='massage'>
+        <h2 v-if="lang=='en'">Massage reservation</h2>
+        <h2 v-else>Zakazivanje masaze</h2>
+        <MassageForm msg="Welcome to Your Vue.js App"/>
+    </div>
+</template>
+
+<script>
+import MassageForm from "../components/services/MassageForm"
+export default {
+    name:"Massage",
+    components:{
+        MassageForm
+    },
+    created(){      
+        if(localStorage.getItem('lang')!=null){
+            this.lang = localStorage.getItem('lang');
+        }
+    }
+}
+
+
+
+</script>
---
+++ src/views/Nutritionist.vue
@@ -0,0 +1,23 @@
+<template>
+     <div class='nutritionist'>
+        <h2 v-if="lang=='en'">Nutritionist reservation</h2>
+        <h2 v-else>Zakazivanje nutricioniste</h2>
+        <NutriForm msg="Welcome to Your Vue.js App"/>
+    </div>
+</template>
+
+
+<script>
+import NutriForm from "../components/services/NutriForm"
+export default {
+    name:"Nutritionist",
+    components:{
+        NutriForm
+    },
+    created(){      
+        if(localStorage.getItem('lang')!=null){
+            this.lang = localStorage.getItem('lang');
+        }
+    }
+}
+</script>
