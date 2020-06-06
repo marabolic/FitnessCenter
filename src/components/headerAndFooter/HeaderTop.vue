4d683ac147bf1541c2961bfb0df7e9e21028c30c
Edvvin <edvvin@mail.com>
2020-06-06 20:20:20 +0200

    Added language buttons

--- src/components/headerAndFooter/HeaderTop.vue
+++ src/components/headerAndFooter/HeaderTop.vue
@@ -1,15 +1,42 @@
 <template>
 <div class="container">
     <div class="text-right">
-        <a>
+        <a @click="setEn()" href="">
             English
             <img src="../../../public/img/en.png">
         </a>
         |
-        <a>
+        <a @click="setSr()" href="">
             Srpski
             <img src="../../../public/img/sr.png">
         </a>
     </div>
 </div>
 </template>
+
+
+<script>
+
+export default {
+	name: 'FilteredTrainingList',
+	components: {
+	},
+	created: function(){
+        this.filter=this.$route.params.filter;
+	},
+	methods:{
+		setEn: function(){
+			localStorage.setItem("lang", "en");
+			window.location.reload(false);
+
+		},
+		setSr: function(){
+			localStorage.setItem("lang", "sr");
+			window.location.reload(false);
+		}
+	},
+	data: function(){
+		return{
+		}
+	}, }
+</script>
