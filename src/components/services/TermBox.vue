db7e7ff5a8facae1aa13169c8aba905d8bd4fcaa
Edvvin <edvvin@mail.com>
2020-06-07 02:12:19 +0200

    Five methods left

--- src/components/services/TermBox.vue
+++ src/components/services/TermBox.vue
@@ -13,10 +13,20 @@
 					</div>
 				</div>
 				<div v-if="showBtn()" class="row">
-					<div class="col-12">
-						<button @click="btnClick()" class="btn btn-primary">
-							Reserve
+					<div v-if="showRedBtn()" class="col-12">
+						<button @click="redBtnClick()" class="btn btn-danger">
+							{{btnText1}}
 						</button>
+					</div>
+					<div v-else class="col-12">
+						<button @click="btnClick()" class="btn btn-primary">
+							{{btnText2}}
+						</button>
+					</div>
+				</div>
+				<div v-else class="col-12">
+					<div v-if="isReserved()" class="col-12">
+						{{btnText3}}
 					</div>
 				</div>
 			</div>
@@ -43,6 +53,20 @@ export default {
 	},
 	created: function(){
 		this.myterm = this.term;
+		let lang = localStorage.getItem("lang");
+		if(lang == null || lang == "undefined"){
+			lang = "sr";
+		}
+		if(lang=="en"){
+			this.btnText1 = "Reserve";
+			this.btnText2 = "Unreserve";
+			this.btnText3 = "Reserved";
+		}
+		else{
+			this.btnText1 = "Rezervisi";
+			this.btnText2 = "Odjavi";
+			this.btnText3 = "Rezervisano";
+		}
 	},
 	props: [
 		"term"
@@ -50,13 +74,24 @@ export default {
 	data: function(){
 		return{
 			myterm: 0,
+			btnText1: "",
+			btnText2: "",
 		}
 	},
 	methods:{
 		showBtn: function(){
+			return true
+		},
+		showRedBtn: function(){
+			return true;
+		},
+		isReserved: function(){
+			return true;
 		},
 		btnClick: function(){
-		}
+		},
+		redBtnClick: function(){
+		},
 	}
 }
 </script>
